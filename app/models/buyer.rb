3b8e54c12145a1ace177af8eebd4a62a78d06cb4
kohei-san <kohei.miyatake@gmail.com>
2021-04-06 19:19:17 +0900

    バリデーション追加（buyer, supplier)

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -3,4 +3,17 @@ class Buyer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presense: true do
+    validates :name,              uniqueness: true
+    validates :phone_number,      numericality: { only_integer: true }, length: { maximum: 11 }
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana,    format: {with: /\A[ア-ン]+\z/}
+    validates :first_name_kana,   format: {with: /\A[ア-ン]+\z/}
+    validates :company_name
+    validates :company_name_kana, format: {with: /\A[ア-ン]+\z/}
+    validates :prefecture_id,     numericality: { other_than: 0 }
+    validates :profile
+  end
 end
