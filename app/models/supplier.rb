1fb6d3416388a5868851393cf432218345dcde56
kohei-san <kohei.miyatake@gmail.com>
2021-04-06 19:34:59 +0900

    partsコントローラー作成、indexルーティング

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -4,16 +4,16 @@ class Supplier < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presense: true do
-    validates :name,              uniqueness: true
-    validates :phone_number,      numericality: { only_integer: true }, length: { maximum: 11 }
-    validates :last_name
-    validates :first_name
-    validates :last_name_kana,    format: {with: /\A[ア-ン]+\z/}
-    validates :first_name_kana,   format: {with: /\A[ア-ン]+\z/}
-    validates :factory_name
-    validates :factory_name_kana, format: {with: /\A[ア-ン]+\z/}
-    validates :prefecture_id,     numericality: { other_than: 0 }
-    validates :profile
-  end
+  # with_options presense: true do
+  #   validates :name,              uniqueness: true
+  #   validates :phone_number,      numericality: { only_integer: true }, length: { maximum: 11 }
+  #   validates :last_name
+  #   validates :first_name
+  #   validates :last_name_kana,    format: {with: /\A[ア-ン]+\z/}
+  #   validates :first_name_kana,   format: {with: /\A[ア-ン]+\z/}
+  #   validates :factory_name
+  #   validates :factory_name_kana, format: {with: /\A[ア-ン]+\z/}
+  #   validates :prefecture_id,     numericality: { other_than: 0 }
+  #   validates :profile
+  # end
 end
