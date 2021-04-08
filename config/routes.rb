312fa61efc6109d3adbc35ca4fde8df58013d596
kohei-san <kohei.miyatake@gmail.com>
2021-04-08 14:52:35 +0900

    HTML追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :suppliers
   devise_for :buyers
-  resources :parts, only: :index
+  root to: "parts#index"
+  resources :parts, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
