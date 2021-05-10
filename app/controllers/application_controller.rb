28e15db5bbed79b83a661390eda0003a594ff69b
kohei-san <kohei.miyatake@gmail.com>
2021-05-10 15:55:12 +0900

    編集、削除機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_account_update_params, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number, :last_name, :last_name_kana, :first_name, :first_name_kana, :company_name, :company_name_kana, :prefecture_id, :profile])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number, :last_name, :last_name_kana, :first_name, :first_name_kana, :company_name, :company_name_kana, :prefecture_id, :profile])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:phone_number, :last_name, :last_name_kana, :first_name, :first_name_kana, :company_name, :company_name_kana, :prefecture_id, :profile])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :suppliers
   devise_for :buyers
   root to: "parts#index"
-  resources :parts, only: [:index, :show, :new, :create]
+  resources :parts
   resources :buyers, only: :show
   resources :suppliers, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
