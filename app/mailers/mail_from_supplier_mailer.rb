06bf9391386d334d1e7fb35e7ac3b42c0e24de10
kohei-san <kohei.miyatake@gmail.com>
2021-06-17 12:18:04 +0900

    メールクラス編集

--- app/mailers/mail_from_supplier_mailer.rb
+++ app/mailers/mail_from_supplier_mailer.rb
@@ -1,2 +1,13 @@
 class MailFromSupplierMailer < ApplicationMailer
+  default :from => 'kohei.miyatake@gmail.com'
+
+  def send_mail_to_buyer(buyer, supplier, part)
+    @buyer = buyer
+    @supplier = supplier
+    @part = part
+    mail(
+      :to => @buyer.email,
+      :subject => "#{@supplier}から#{@part}についての問い合わせです。"
+    )
+  end
 end
